73b5192cef8faa801c902365c06ace5d8a3c735d
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-15 11:32:58 +0900

    %{name}でtメソッドで値を渡せるように変更 + 削除確認ダイアログの翻訳を追加

--- config/application.rb
+++ config/application.rb
@@ -12,11 +12,8 @@ module BooksApp
     config.load_defaults 6.1
 
     I18n.available_locales = [:en, :ja]
-
     I18n.default_locale = :ja
-
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-
 
     # Configuration for the application, engines, and railties goes here.
     #
