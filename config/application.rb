ba5ca672218b966564743f62018cbc0c661991f1
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-16 16:09:58 +0900

    config/initializers/locale.rbで言語設定が読み込まれているので、不要な箇所を削除

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,6 @@ module BooksApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    I18n.available_locales = [:en, :ja]
-    I18n.default_locale = :ja
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
