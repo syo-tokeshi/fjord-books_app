3a406478c3d377f2411de69abd9a529355372109
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-17 11:32:44 +0900

    不要なbootstrapの読み込みの削除 + ファイルの並びの整形 + i18nの翻訳を微調整

--- db/migrate/20230212034838_add_devise_to_users.rb
+++ db/migrate/20230212034838_add_devise_to_users.rb
@@ -5,13 +5,13 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       ## Database authenticatable
       t.string :zip_code,              null: false, default: ""
-      t.string :prefecture_code, null: false, default: ""
-      t.string :city, null: false, default: ""
-      t.string :street, null: false, default: ""
+      t.string :prefecture_code,       null: false, default: ""
+      t.string :city,                  null: false, default: ""
+      t.string :street,                null: false, default: ""
       t.string :other_address
-      t.text :introduction,              null: false, default: ""
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.text :introduction,            null: false, default: ""
+      t.string :email,                 null: false, default: ""
+      t.string :encrypted_password,    null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
