e9531cdb15189875d5874e2828314b607d35a16d
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-20 17:39:43 +0900

    間違って作ったmigrationファイルを取り消す

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_20_011440) do
+ActiveRecord::Schema.define(version: 2023_02_20_005925) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 2023_02_20_011440) do
     t.string "postal_code"
     t.string "address"
     t.text "self_introduction"
-    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
