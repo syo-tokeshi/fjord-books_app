f4a68dd1ee46b867c7b1e05a50b581dfa008157d
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-22 18:08:52 +0900

    userモデルにuser_relationshipsとの自己結合をするための関連付け + 制約を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :active_friendships,  class_name: 'UserRelationship',
+           foreign_key: :follower_id,
+           dependent: :destroy,
+           inverse_of: :follower
+
+  has_many :passive_friendships, class_name: 'UserRelationship',
+           foreign_key: :followed_id,
+           dependent: :destroy,
+           inverse_of: :followed
+
+  has_many :followings, through: :active_friendships, source: :followed
+  has_many :followers, through: :passive_friendships, source: :follower
 end
