bbde9061049a3bc0df8b3a7164d44276201b2e24
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-22 18:17:15 +0900

    FriendshipsControllerを作成し、フォロー&アンフォローのアクションを追加

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,19 @@
+class FriendshipsController < ApplicationController
+  before_action :set_user
+
+  def create
+    current_user.follow(@user)
+    redirect_to user_path(@user), notice: t('controllers.common.notice_create', name: Friendship.model_name.human)
+  end
+
+  def destroy
+    current_user.unfollow(@user)
+    redirect_to user_path(@user), notice: t('controllers.common.notice_destroy', name: Friendship.model_name.human)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+end
