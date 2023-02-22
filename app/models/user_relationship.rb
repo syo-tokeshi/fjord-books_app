aae4858f34ad253fb3af4f889e6841e6dfbba5e7
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-22 18:05:34 +0900

    user_relationshipsテーブルの作成 + それに伴う制約の初期設定

---
+++ app/models/user_relationship.rb
@@ -0,0 +1,7 @@
+class UserRelationship < ApplicationRecord
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
+
+  validates :follower_id, uniqueness: { scope: :followed_id }
+  validates :follower_id, :followed_id, presence: true
+end
