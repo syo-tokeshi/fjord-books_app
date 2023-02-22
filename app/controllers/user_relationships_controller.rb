11d74dba8b0fa335fc270ff9889e42ead324dd8b
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-22 20:04:17 +0900

    ユーザーをフォロー&アンフォローした際のflashメッセージを変更

--- app/controllers/user_relationships_controller.rb
+++ app/controllers/user_relationships_controller.rb
@@ -3,12 +3,12 @@ class UserRelationshipsController < ApplicationController
 
   def create
     current_user.follow(@user)
-    redirect_to user_path(@user), notice: t('controllers.common.notice_create', name: UserRelationship.model_name.human)
+    redirect_to user_path(@user), notice: t('controllers.follow.notice_create')
   end
 
   def destroy
     current_user.unfollow(@user)
-    redirect_to user_path(@user), notice: t('controllers.common.notice_destroy', name: UserRelationship.model_name.human)
+    redirect_to user_path(@user), notice: t('controllers.follow.notice_destroy')
   end
 
   private
