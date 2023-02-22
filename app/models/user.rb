c5d29fbcce4b6bcf29c86669655605ac2d71be27
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-22 18:27:34 +0900

    フォローしているか確認するメソッド + フォローした際のcreate & destroyのメソッドを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,30 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
-  has_many :active_friendships,  class_name: 'UserRelationship',
+  has_many :active_relationships,  class_name: 'UserRelationship',
            foreign_key: :follower_id,
            dependent: :destroy,
            inverse_of: :follower
 
-  has_many :passive_friendships, class_name: 'UserRelationship',
+  has_many :passive_relationships, class_name: 'UserRelationship',
            foreign_key: :followed_id,
            dependent: :destroy,
            inverse_of: :followed
 
-  has_many :followings, through: :active_friendships, source: :followed
-  has_many :followers, through: :passive_friendships, source: :follower
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  def following?(other_user)
+    active_relationships.where(followed_id: other_user.id).exists?
+  end
+
+  def follow(other_user)
+    active_relationships.find_or_create_by!(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    friendship = active_relationships.find_by(followed_id: other_user.id)
+    friendship.destroy! if !!friendship
+  end
+
 end
