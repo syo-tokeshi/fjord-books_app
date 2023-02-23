954c66559cef329b008bc8de74fec578964b78a2
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-24 08:56:28 +0900

    orderメソッドの引数の値をシンボルに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 
 class UsersController < ApplicationController
   def index
-    @users = User.order(id: 'desc').page(params[:page]).per(10)
+    @users = User.order(id: :desc).page(params[:page]).per(10)
   end
 
   def show
