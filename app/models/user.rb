b84ed31c0f04db638ce39be2108d5b0f87629d0d
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-26 23:39:12 +0900

    使われていないthumbの削除 + それに伴う変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :avatar do |attachable|
-    attachable.variant :thumb, resize_to_limit: [100, 100]
-  end
+  has_one_attached :avatar
 
   validates :avatar, processable_image: true, content_type: ['image/png', 'image/jpeg', 'image/gif']
 end
