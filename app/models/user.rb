b985fb2b518fe6f6add3b1ed884ade695f103b6f
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-27 11:36:58 +0900

    コメントに、投稿したユーザーの名前かメアドが表示されるメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,8 @@ class User < ApplicationRecord
     relationship = active_relationships.find_by(following_id: user.id)
     relationship&.destroy!
   end
+
+  def name_or_email
+    name.any? ? name : email
+  end
 end
