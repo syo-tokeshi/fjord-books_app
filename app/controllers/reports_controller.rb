623e0a7b28e3d015f687bbd4a816b56e0a7a271f
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-02-27 13:05:41 +0900

    漏れていたレポートの翻訳を追加

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -22,7 +22,7 @@ class ReportsController < ApplicationController
     @report = Report.new(report_params)
     @report.user_id = current_user.id
     if @report.save
-      redirect_to report_url(@report), notice: 'Report was successfully created.'
+      redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
       render :new, status: :unprocessable_entity
     end
@@ -30,7 +30,7 @@ class ReportsController < ApplicationController
 
   def update
     if @report.update(report_params)
-      redirect_to report_url(@report), notice: 'Report was successfully updated.'
+      redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
     end
@@ -38,7 +38,7 @@ class ReportsController < ApplicationController
 
   def destroy
     @report.destroy
-    redirect_to reports_url, notice: 'Report was successfully destroyed.'
+    redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
   private
