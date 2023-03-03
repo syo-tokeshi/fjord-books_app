39b1c06deb815df6f09d0344c8b29e055f127765
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-03-03 11:29:16 +0900

    レポートモデルのインスタンスメソッドのテスト + テスト用データを作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @report_written_by_taro = reports(:report_written_by_taro)
+    @taro = users(:taro)
+    @hanako = users(:hanako)
+  end
+
+  test "editable?" do
+    assert_equal false, @report_written_by_taro.editable?(@hanako)
+    assert_equal true, @report_written_by_taro.editable?(@taro)
+  end
+
+  test "created_on" do
+    assert_equal Date.today, @report_written_by_taro.created_on
+  end
 end
