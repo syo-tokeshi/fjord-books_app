1f964f777a301961f4f8451e2ac7c9c111e92b9b
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-03-03 12:20:07 +0900

    日報を書くフローのシステムテストを作成 + それに伴うデータのパスワードを変更

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -1,9 +1,24 @@
 require "application_system_test_case"
 
 class ReportsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit reports_url
-  #
-  #   assert_selector "h1", text: "Report"
-  # end
+  setup do
+    @report_written_by_taro = reports(:report_written_by_taro)
+    @taro = users(:taro)
+    @hanako = users(:hanako)
+  end
+  test "visiting the index" do
+    visit root_path
+
+    fill_in 'Eメール', with: @taro.email
+    fill_in 'パスワード', with: "111111"
+    click_on 'ログイン'
+
+    visit new_report_url
+
+    fill_in 'タイトル', with: '晴れが好き'
+    fill_in '内容', with: '明日晴れたらな'
+    click_on '登録する'
+
+    assert_text '日報が作成されました。'
+  end
 end
