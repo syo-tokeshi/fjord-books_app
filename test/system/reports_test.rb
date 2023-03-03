84d5b0532e2142ee366a396f59c2741497955311
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-03-03 13:14:31 +0900

    レポートの「更新 + 削除」のテストも追加

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -5,13 +5,15 @@ class ReportsTest < ApplicationSystemTestCase
     @report_written_by_taro = reports(:report_written_by_taro)
     @taro = users(:taro)
     @hanako = users(:hanako)
-  end
-  test "visiting the index" do
+
     visit root_path
 
     fill_in 'Eメール', with: @taro.email
     fill_in 'パスワード', with: "111111"
     click_on 'ログイン'
+  end
+
+  test "create report" do
 
     visit new_report_url
 
@@ -20,5 +22,27 @@ class ReportsTest < ApplicationSystemTestCase
     click_on '登録する'
 
     assert_text '日報が作成されました。'
+  end
+
+  test "update report" do
+
+    visit edit_report_url(@report_written_by_taro.id)
+
+    fill_in 'タイトル', with: '晴れが嫌い'
+    fill_in '内容', with: '明日雨ならいいな'
+    click_on '更新する'
+
+    assert_text '日報が更新されました。'
+  end
+
+  test "delete report" do
+
+    visit reports_url
+
+    page.accept_confirm do
+      click_on '削除', match: :first
+    end
+
+    assert_text '日報が削除されました。'
   end
 end
