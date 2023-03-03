815ffaaee926a869a73c0ed234131c099cf71cf9
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-03-03 09:55:17 +0900

    ユーザーモデルのインスタンスメソッドのテスト + テスト用データを作成

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,31 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @taro = users(:taro)
+    @hanako = users(:hanako)
+  end
+
+  test "太郎が花子をフォローしているか確かめる" do
+    assert_equal  true, @taro.following?(@hanako)
+  end
+
+  test "花子が太郎にフォローされてるか確かめる" do
+    assert_equal  true, @hanako.followed_by?(@taro)
+  end
+
+  test "花子は太郎をフォロー出来るか確かめる" do
+    assert @hanako.follow(@taro)
+  end
+
+  test "太郎が花子をアンフォロー出来るか確かめる" do
+    assert @taro.unfollow(@hanako)
+  end
+
+  test "太郎の名前があれば名前を、なければメアドを返す" do
+    assert_equal "太郎", @taro.name_or_email
+    @taro.name = nil
+    assert_equal "taro@example.com", @taro.name_or_email
+  end
+
 end
